27d616f9e9f03315e679fad4894e0c095fa6f03f
Benjamin Caldwell <bcaldwell@atlassian.com>
2024-11-18 10:25:50 +1300

    Added NVM zsh config and also updated the ruff lsp config.

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -97,7 +97,7 @@ cmp.setup({
 -- Configure `ruff-lsp`.
 -- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
 -- For the default config, along with instructions on how to customize the settings
-require('lspconfig').ruff_lsp.setup {
+require('lspconfig').ruff.setup {
   init_options = {
     settings = {
       -- Any extra CLI arguments for `ruff` go here.
--- readme.md
+++ readme.md
@@ -61,6 +61,13 @@ brew install wget
 ```
 
 ##### Node Installation
+
+```
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+```
+
 ```
 nvm install --lts
 ```
