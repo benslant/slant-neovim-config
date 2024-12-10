b3b110a1f3f3da71150736af5e74b0020e268a16
Benjamin Caldwell <bcaldwell@atlassian.com>
2024-12-10 20:50:44 +1300

    Adding dadbof config

--- lua/dap-config.lua
+++ lua/dap-config.lua
@@ -1,6 +1,6 @@
 -- dap configuration
 vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
-vim.keymap.set('n', '<F17>', function() require('dap').disconnect() end)
+vim.keymap.set('n', '<F13>', function() require('dap').disconnect() end)
 vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
 vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
 vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
