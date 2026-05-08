007e42008da2ec32598cf55577c7c0b44a48f39e
Benjamin Caldwell <bcaldwell@atlassian.com>
2026-05-08 17:09:00 +1200

    Work laptop fix

--- lua/lazy-config.lua
+++ lua/lazy-config.lua
@@ -28,7 +28,7 @@ require('lazy').setup({
 	{'hrsh7th/cmp-nvim-lsp'},
 	{'hrsh7th/nvim-cmp'},
 	{'folke/tokyonight.nvim'},
-	{"nvim-treesitter/nvim-treesitter", config = function() require("nvim-treesitter.config").setup {
+	{"nvim-treesitter/nvim-treesitter", config = function() require("nvim-treesitter").setup {
 			highlight = {
 				enable = true,
 			},
